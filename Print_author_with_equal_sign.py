30f4dc5d9b84fb4fa31c2901cc6dc6ff7eb0b147
Nisar Laftissi <107173728+NisarLDev@users.noreply.github.com>
2023-05-20 07:44:29 +0200

    Added Print author

---
+++ Print_author_with_equal_sign.py
@@ -0,0 +1,24 @@
+# My solution
+
+line = '=' * 40
+author_text = "author: "
+author_email = "johnsmith@sample.com"
+date_text = "date: "
+date = "01-01-2021"
+print(line)
+print(author_text + author_email)
+print(date_text + date)
+print(line)
+
+# Solution of the instructor
+
+
+print('=' * 40)
+print('author: johnsmith@sample.com')
+print('date: 01-01-2021')
+print('=' * 40)
+
+
+
+#END OF CODE
+#END OF FILE
