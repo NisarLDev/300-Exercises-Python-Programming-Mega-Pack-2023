086e40ec10ee9fd966f6e816df2a27c699e92685
Nisar Laftissi <107173728+NisarLDev@users.noreply.github.com>
2023-05-20 06:38:27 +0200

    Update Print-Python-Version-writing.py

--- 2 - Print function./Print-Python-Version-writing.py
+++ 2 - Print function./Print-Python-Version-writing.py
@@ -1,3 +1,15 @@
+import sys
+
+ # My solution
+
+learning = "I am learning "
+py = "Python "
+vers = "version "
+version_py = ".".join(map(str, sys.version_info[:2]))
+print(learning+py+vers+version_py)
+
+# Solution of the instructor
+
 lang = 'Python'
 version = '3.8'
 print(f'I am learning {lang} version {version}')
