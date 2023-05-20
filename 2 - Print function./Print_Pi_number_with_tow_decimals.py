1c79f84d8ed9439ab54dc1e6bea8297398c00e72
Nisar Laftissi <107173728+NisarLDev@users.noreply.github.com>
2023-05-20 07:07:12 +0200

    Added Print Pi number with tow decimals

---
+++ 2 - Print function./Print_Pi_number_with_tow_decimals.py
@@ -0,0 +1,17 @@
+
+
+
+# My solution
+
+print('Learn Python!')
+number_pi = 3.1415926535
+print(f"Pi: {number_pi:.2f}")
+
+
+# Solution of the instructor
+
+pi = 3.1415926535
+print(f'Pi: {pi:.2f}')
+
+#END OF CODE
+#END OF FILE
