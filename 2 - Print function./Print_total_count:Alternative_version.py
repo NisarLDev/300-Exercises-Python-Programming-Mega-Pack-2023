3421e02ff1ca91c3e516e6d0437a2ea3e2484e87
Nisar Laftissi <107173728+NisarLDev@users.noreply.github.com>
2023-05-20 06:45:59 +0200

    Update Print_total_count:Alternative_version.py

--- 2 - Print function./Print_total_count:Alternative_version.py
+++ 2 - Print function./Print_total_count:Alternative_version.py
@@ -1,3 +1,16 @@
+# My solution
+
+count = 69.99
+price = count
+print(f'This costs {price}')
+
+#
+
+price = 69.99
+print(f'This costs {price}')
+
+# Solution of the instructor
+
 price = 69.99
 print(f'This costs {price}')
 #END OF FILE
