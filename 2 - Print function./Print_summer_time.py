933d48a3db1f371df3b30537450d66e33d11ba4e
Nisar Laftissi <107173728+NisarLDev@users.noreply.github.com>
2023-05-20 08:12:07 +0200

    Create Print_summer_time.py

---
+++ 2 - Print function./Print_summer_time.py
@@ -0,0 +1,15 @@
+# My solution
+
+print("summer", "time", "holiday", sep="#")
+
+# OR
+a="#"
+print(f'summer{a}time{a}holiday')
+
+# Solution of the instructor
+
+print('summer', 'time', 'holiday', sep='#')
+
+
+#END OF CODE
+#END OF FILE
