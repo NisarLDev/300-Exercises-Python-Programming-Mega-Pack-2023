a3ade22b85678cbebdce9ae461aa9b7af719b67a
Nisar Laftissi <107173728+NisarLDev@users.noreply.github.com>
2023-05-20 06:56:00 +0200

    Update Product_price function

--- 2 - Print function./Print_price_and_weight.py
+++ 2 - Print function./Print_price_and_weight.py
@@ -1,3 +1,16 @@
+# My solution
+
 product_price = 34.99
 product_weight = 20
 print(f'Price: ${product_price}. Weight: {product_weight} lbs.')
+
+# Solution of the instructor
+
+price = 34.99
+weight = 20
+     
+print(f'Price: ${price}. Weight: {weight} lbs.')
+
+#END OF CODE
+#END OF FILE
+
