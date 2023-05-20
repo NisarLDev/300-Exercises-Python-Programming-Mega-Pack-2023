aec4eadf3e30664dd4f8d7b726cf052362c32e51
Nisar Laftissi <107173728+NisarLDev@users.noreply.github.com>
2023-05-20 08:47:14 +0200

    Update 01_Python-Version-on-System.py

--- 1 - Starter. Python Version on System/01_Python-Version-on-System.py
+++ 1 - Starter. Python Version on System/01_Python-Version-on-System.py
@@ -1,9 +1,14 @@
 import sys
- 
- 
-print(sys.version.split()[0])
-
-# OR
+# My solution
 
 version = ".".join(map(str, sys.version_info[:3]))
 print(version)
+
+
+# Solution of the instructor
+ 
+print(sys.version.split()[0])
+
+
+#END OF CODE
+#END OF FILE
