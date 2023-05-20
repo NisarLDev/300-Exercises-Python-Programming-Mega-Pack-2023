f14953e2439d03344e54121d66610ccf38929688
Nisar Laftissi <107173728+NisarLDev@users.noreply.github.com>
2023-05-20 07:32:52 +0200

    Added Print_version_with_lines.py

---
+++ Print_version_with_lines.py
@@ -0,0 +1,17 @@
+# My solution
+
+line = '-' * 40
+version = "VERSION: 1.0.1"
+print(line)
+print(version)
+print(line)
+
+
+# Solution of the instructor
+
+print('-' * 40)
+print('VERSION: 1.0.1')
+print('-' * 40)
+
+#END OF CODE
+#END OF FILE
